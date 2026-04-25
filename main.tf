a2dbc6a17cfacb779b24c504e3c885ddc6e5b6e6
Rakshitha S <rakshi69292@gmail.com>
2026-04-25 15:50:51 +0530

    Update key_name for AWS instance resource

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "rakshi" {
   ami           = "ami-0e12ffc2dd465f6e4"
   instance_type = "t3.micro"
   subnet_id = "subnet-0f84c7e8c9f149767"
-  key_name = "ranjani"
+  key_name = "ranjani_key"
   security_groups = ["sg-0bde0ee044fe82f14"]
 tags = {
  Name = "rakshi-ranjani"
