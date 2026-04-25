76b39f8b98cb9d085cf1cca6e5e10af65da3a52e
Rakshitha S <rakshi69292@gmail.com>
2026-04-25 15:27:44 +0530

    Add AWS provider and EC2 instance configuration

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = "ap-south-1"
+}
+
+resource "aws_instance" "rakshi" {
+  ami           = "ami-0e12ffc2dd465f6e4"
+  instance_type = "t3.micro"
+  subnet_id = "subnet-0f84c7e8c9f149767"
+  key_name = "ranjani"
+  security_groups = ["sg-0bde0ee044fe82f14"]
+tags = {
+ Name = "rakshi-ranjani"
+}
+}
